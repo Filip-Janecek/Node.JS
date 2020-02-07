5c227ff61730f93c6dc07e03294f80171c41e44b
Filip Janeček <filip.janecek28gmail.com>
2020-02-07 17:11:25 +0100

    Vlastní modul

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const objem = require('./mujprvnimodul');
-console.log("Objem koule je " + objem.koule(4) + " metrů krychlových a objem krychle je " + objem.krychle(4) + " metrů krychlových.");
+console.log("Objem koule je " + objem.koule(4)
+    + " metrů krychlových a objem krychle je "
+    + objem.krychle(4) + " metrů krychlových.");
