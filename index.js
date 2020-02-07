99e94c3308745753d0ba4da83f9e0e71e310a2a4
Filip Janeček <filip.janecek28gmail.com>
2020-02-07 16:29:19 +0100

    Vlastní modul

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-let a = 1;
-let b = 2;
-let soucet = a + b;
-console.log("Součtem čísel " + a + " a " + b + " je "+ soucet);
-console.log(`Součtem čísel ${a} a ${b} je ${soucet}.`);
+const objem = require('./mujprvnimodul');
+console.log("Objem koule je " + objem.koule(4) + " metrů krychlových a objem krychle je " + objem.krychle(4) + " metrů krychlových.");
---
+++ mujprvnimodul.js
@@ -0,0 +1,7 @@
+exports.koule = function (r) {
+  return 4/3 * Math.PI * Math.pow(r, 3);
+};
+
+ exports.krychle = function (a) {
+     return Math.pow(a,3);
+ };
